352e5b3abb0b45656f2edfae432ac186463cacbb
epqlffltm <dsfake1225@gmail.com>
2025-08-15 15:48:18 +0900

    2025-08-15

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+
+from posts.forms import CommentForm
 from posts.models import Post
+from django.views.decorators.http import require_POST
 
 def feeds(request):
     if not request.user.is_authenticated:
@@ -9,5 +12,20 @@ def feeds(request):
     print("user:", user)
     print("is_authenticated:", is_authenticated)
     posts=Post.objects.all()
-    context={"posts":posts}
+    comment_form=CommentForm()
+    context={"posts":posts,"comment_form":comment_form,}
     return render(request,"posts/feeds.html", context)
+
+@require_POST
+def comment_add(request):
+    print(request.POST)
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.user = request.user
+        comment.save()
+        print(comment.id)
+        print(comment.content)
+        print(comment.user)
+
+    return redirect("/posts/feeds/")
