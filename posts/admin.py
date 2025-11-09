ea3910d853ae0f2301bae7d3188fa7c50e370435
epqlffltm <dsfake1225@gmail.com>
2025-11-10 00:14:01 +0900

    like 기능 구현

--- posts/admin.py
+++ posts/admin.py
@@ -15,14 +15,21 @@ class PostImageInline(admin.TabularInline):
     model = PostImage
     extra = 1
 
-
+class LikeUserInline(admin.TabularInline):
+    model = Post.like_users.through
+    verbose_name = "좋아요 한 User"
+    verbose_name_plural = f"{verbose_name} 목록"
+    extra = 1
+    
+    def has_change_permission(self, request, obj = None):
+        return False
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         "id",
         "content",
     ]
-    inlines = [CommentInline, PostImageInline]
+    inlines = [CommentInline, PostImageInline, LikeUserInline,]
     formfield_overrides = {ManyToManyField: {"widget": CheckboxSelectMultiple},}
 
 
@@ -33,7 +40,7 @@ class PostImageAdmin(admin.ModelAdmin):
         "post",
         "photo",
     ]
-
+    
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
